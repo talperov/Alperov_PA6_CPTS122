8cb057d66a42cf9fc8489f1395393c7b74b97038
Anthony Alperov <talperov10@gmail.com>
2025-10-20 10:36:36 -0700

    Add files via upload

---
+++ Alperov_PA6/Alperov_PA6/Alperov_PA6/Alperov_PA6/BST.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "BSTNode.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+class BST
+{
+private:
+
+	BSTNode* pRoot;
+
+	// Helpers and Recursive functions
+	void insertRec(BSTNode* pNode, char English, string Morse);
+	string searchRec(BSTNode* pNode, char Searching) const;
+	void printRec(BSTNode* pNode);
+	void deleteRec(BSTNode* pNode);
+
+public:
+	
+	// Constructors & Destructor
+	BST();
+	~BST();
+
+	// Members
+	bool isEmpty() const;
+	void insert(char English, string Morse);
+	string search(char Searching) const;
+	void print();
+	void openFile(string& infile);
+};
+
+
